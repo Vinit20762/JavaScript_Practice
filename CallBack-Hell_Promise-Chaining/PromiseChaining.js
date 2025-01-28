2863faef56f5b23449b5a2035ff0f945f1797838
Fragger <144892877+Vinit20762@users.noreply.github.com>
2025-01-28 16:56:28 +0000

    Promise chaining Program

---
+++ CallBack-Hell_Promise-Chaining/PromiseChaining.js
@@ -0,0 +1,39 @@
+/* Q: Write code that
+logs hi after 1 second
+logs hello 3 seconds after step 1
+logs hello there 5 seconds after step 2 */
+
+// Using Promise Chaining
+
+function setTimeoutPromisified(duration){
+    return new Promise(function(resolve){
+      setTimeout(resolve, duration);
+    });
+}
+
+setTimeoutPromisified(1000).then(function(){
+    console.log("hi");
+
+    setTimeoutPromisified(3000).then(function(){
+        console.log("hello");
+
+        setTimeoutPromisified(5000).then(function(){
+            console.log("hello there")
+        })
+    })
+})
+
+
+// cleaner way of promise chaining
+
+// setTimeoutPromisified(1000).then(function(){
+//     console.log("Hi")
+//     return setTimeoutPromisified(3000)
+// }).then(function(){
+//     console.log("hello")
+//     return setTimeoutPromisified(5000)
+// }).then(function(){
+//     console.log("hello there")
+// })
+
+
