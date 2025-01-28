35c2e16b56ad6882ae77c36db777cf1aaabb3329
Fragger <144892877+Vinit20762@users.noreply.github.com>
2025-01-28 16:56:06 +0000

    CallBack Hell program

---
+++ CallBack-Hell_Promise-Chaining/CallBack.js
@@ -0,0 +1,21 @@
+/* Q: Write code that
+logs hi after 1 second
+logs hello 3 seconds after step 1
+logs hello there 5 seconds after step 2 */
+
+
+//Callback hell program using anonymous funciton.
+function Callback(){
+  console.log("hi");
+}
+setTimeout(function(){ 
+ console.log("hi");
+
+ setTimeout(function(){
+    console.log("Hello");
+
+  setTimeout(function(){
+    console.log("Hello there");
+  }, 5000);
+ }, 3000);
+}, 1000);  
